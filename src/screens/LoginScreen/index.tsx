436f13cc171b69ddfff97e21818faf6657e9e574
Truong Cao <conmeobeou332000@gmail.com>
2023-04-20 09:20:06 +0700

    Setup environment

---
+++ src/screens/LoginScreen/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@rneui/themed';
+import React from 'react';
+import { View } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login Page</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
